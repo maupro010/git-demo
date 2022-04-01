5d459579dbc3a8107b68cc61be27245afc253a86
Phuc Le Hoang <phuc51mta@gmail.com>
2022-04-01 16:15:23 +0700

    add form.php file

---
+++ form.php
@@ -0,0 +1,15 @@
+<?php
+
+class User
+{
+    private $username;
+    private $password;
+    private static $passwordLength = 5;
+
+    public static function getPasswordLength()
+    {
+        return self::$passwordLength;
+    }
+}
+
+echo User::getPasswordLength();
